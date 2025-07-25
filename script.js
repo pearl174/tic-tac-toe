d27591c4258fcbc02b1a600ec80f010ed60df006
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-25 22:35:19 +0530

    Add gameBoard as IIFE with createBoard, getBoard, changeBoard. changeBoard might have validation added later. Add Player factory function. Add gameController as IIFE. So far it calls createBoard and constructs player1 and 2.

---
+++ script.js
@@ -0,0 +1,34 @@
+const gameBoard = (function () {
+    const board = [];
+    const createBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            const temp = [];
+            for (let j = 0; j < 3; j++) {
+                temp.push("");
+            }
+            board.push(temp);
+        }
+    }
+
+    
+    const getBoard = () => board;
+
+    const changeBoard = (x, marker) => {
+        const i = Math.floor(x / 3);
+        const j = x % 3;
+        if (board[i][j] === "") 
+            board[i][j] = marker;
+    }
+
+    return { createBoard, getBoard, changeBoard }
+}) ();
+
+const Player = (name, marker) => { name, marker }
+
+const gameController = (function () {
+    gameBoard.createBoard();
+
+    const player1 = players("A", "X");
+    const player2 = players("B", "O");
+
+})
