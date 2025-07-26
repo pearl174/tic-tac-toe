f63342e524c9b221ba8c2d4cee8bd6c13d59179f
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-26 15:14:46 +0530

    Add isBoardFull, change changeBoard to take x and y, correct Player to return actual object, add getPlayerTurn, switchPlayerTurn, makeMove, checkTie, checkWin

--- script.js
+++ script.js
@@ -13,22 +13,63 @@ const gameBoard = (function () {
     
     const getBoard = () => board;
 
-    const changeBoard = (x, marker) => {
-        const i = Math.floor(x / 3);
-        const j = x % 3;
-        if (board[i][j] === "") 
-            board[i][j] = marker;
+    const changeBoard = (x, y, marker) => {
+        if (board[x][y] === "") 
+            board[x][y] = marker;
+    }
+
+    const isBoardFull = () => {
+        return board.every(row => row.every(cell => cell !== ""));
     }
 
     return { createBoard, getBoard, changeBoard }
 }) ();
 
-const Player = (name, marker) => { name, marker }
+const Player = (name, marker) => ({ name, marker });
 
 const gameController = (function () {
     gameBoard.createBoard();
 
-    const player1 = players("A", "X");
-    const player2 = players("B", "O");
+    const player1 = Player("A", "X");
+    const player2 = Player("B", "O");
 
-})();
+    let playerTurn = player1;
+
+    const getPlayerTurn = () => playerTurn;
+
+    const switchPlayerTurn = () => {
+        playerTurn = (playerTurn === player1) ? player2 : player1;
+    }
+
+    const makeMove = (x, y) => {
+        const board = gameBoard.getBoard();
+        if (board[x][y] === "") {
+            board.changeBoard(x, y, getPlayerTurn().marker);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    const checkTie = () => {
+        return gameBoard.isBoardFull();
+    }
+
+    const checkWin = () => {
+        const board = gameBoard.getBoard();
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+            return board[1][1];
+        }
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                return board[i][0];
+            } 
+            else if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                return board[0][i];
+            }
+        }
+        return "";
+    }
+})
+
